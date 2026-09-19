0aad2720519d01917f5f8c125627ea959c4abde2
Jack Greiner <jack@emoss.org>
2026-09-19 15:50:57 -0400

    chore: bump version to 4.0.9
    - io_scene_kotor/blender_manifest.toml: extension version
    - io_scene_kotor/__init__.py: bl_info version for the add-on zip

--- io_scene_kotor/__init__.py
+++ io_scene_kotor/__init__.py
@@ -110,7 +110,7 @@ from .ui.props.scene import ScenePropertyGroup
 bl_info = {
     "name": "KotorBlender",
     "author": "Symmetric & Purifier & Ndix UR & seedhartha & OpenKotOR",
-    "version": (4, 0, 8),
+    "version": (4, 0, 9),
     "blender": (2, 83),
     "location": "File > Import-Export, Object Properties",
     "description": "Import, edit and export KotOR models",
